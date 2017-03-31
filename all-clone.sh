28c54ead13e8b96c615c1100412534c636af20bc
Angie the Fairytale <angie@Angies-MBP.lan>
2017-03-31 11:15:02 +0200

    add all-clone script

---
+++ all-clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ..
+git clone https://github.com/vizabi/vizabi.git vizabi
+git clone https://github.com/vizabi/vizabi-barrankchart.git vizabi-barrankchart
+git clone https://github.com/vizabi/vizabi-bubblechart.git vizabi-bubblechart
+git clone https://github.com/vizabi/vizabi-mountainchart.git vizabi-mountainchart
+git clone https://github.com/vizabi/vizabi-linechart.git vizabi-linechart
+git clone https://github.com/vizabi/vizabi-cartogram.git vizabi-cartogram
+git clone https://github.com/vizabi/vizabi-popbyage.git vizabi-popbyage
+git clone https://github.com/vizabi/vizabi-extapimap.git vizabi-extapimap
+git clone https://github.com/vizabi/vizabi-bubblemap.git vizabi-bubblemap
+git clone https://github.com/vizabi/vizabi-barchart.git vizabi-barchart
+cd vizabi-preview
