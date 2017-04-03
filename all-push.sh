1d84c67de03e96502a1e535ba73924ece617ff85
angieskazka <konzeptmeister@gmail.com>
2017-04-03 14:00:19 +0200

    Add push script

---
+++ all-push.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+(cd ../vizabi-barrankchart; git push)
+(cd ../vizabi-bubblechart; git push)
+(cd ../vizabi-mountainchart; git push)
+(cd ../vizabi-linechart; git push)
+(cd ../vizabi-cartogram; git push)
+(cd ../vizabi-popbyage; git push)
+(cd ../vizabi-extapimap; git push)
+(cd ../vizabi-bubblemap; git push)
+(cd ../vizabi-barchart-ds; git push)
