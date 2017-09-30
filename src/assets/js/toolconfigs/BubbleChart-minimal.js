4b7e018451633c3c95949d4fcf593a83df61d387
angieskazka <konzeptmeister@gmail.com>
2017-09-30 22:15:03 +0200

    Convenient list to try things

--- src/assets/js/toolconfigs/BubbleChart-minimal.js
+++ src/assets/js/toolconfigs/BubbleChart-minimal.js
@@ -1,6 +1,50 @@
 var VIZABI_MODEL = {
+// state: {
+//   time: {
+//     unit: "quarter"
+//   }
+// },
   data: {
-    reader: 'waffle', 
-    path: 'https://waffle-server-dev.gapminderdev.org/api/ddf/ql'
+    reader: 'csv', 
+//   reader: 'csv-time_in_columns', 
+    path: 'data/csv-broken/columns-messed-up.csv'
   }
 };
+
+
+//timeformat-onlyoneyear--delia.csv
+//no-string-concepts.csv
+//punctuation-tricky--naturkatastrofer.csv
+//migrant--native-geo--3measures.csv
+//timeformat-month.csv
+//basic.csv
+//strings-change-over-time.csv
+//migrant--gapminder-geo--1measure.csv
+//punctuation-semicolon--battledeaths.csv
+//punctuation-semicolon.csv
+//timeright--empty-columns.csv
+//timeright--empty-lines--finsam.csv
+//timeright--improved water by income.csv
+//timeright--access to electrity.csv
+//timeright--migrant--native-geo--2measures--win-1252.csv
+//timeright--migrants-native-geo--1measure--utf8.csv
+//timeright--lines-with-no-data--geof.csv
+//timeright--education.csv
+//timeright--timeright--line-breaks.csv
+//timeright--vaccines.csv
+//timeright--Girls and boys going to school.csv
+//timeright--emissions-vs-renewables.csv
+//timeright--Child mortality rate.csv
+//timeright--missing-indicator-columns--fertility.csv
+//timeright-timeformat-quarter.csv
+
+
+//columns-messed-up.csv
+//two-dimensisons--energyflows.csv
+//timefromat-bad--merja.csv
+//timeformat-none--delia.csv
+//timeright--different-delimiters-in-one-file.csv
+//timeright--excel-broken-encoding.csv
+//timeright--repeated-keys--vaccines.csv
+//timeright--quoted-numbers--geof.csv
+//timeright--broken-csv--education.csv
