8bb91b014416f2b4be274aebc0f059d4bbdffe3d
Dmitry Basov <dmbasov@gmail.com>
2018-01-24 22:11:54 +0700

    update  csvreader tests to puppeteer api v1 (#32)

--- tests/csvreader.js
+++ tests/csvreader.js
@@ -4,8 +4,10 @@ const host = "http://static.gapminderdev.org/preview/read-from-csv";
 const srcPath = "src/";
 const csvFolder = 'csv';
 const csvBrokenFolder = 'csv-broken';
-const regExp = /(?:(timeright)|timeformat-(.+)\W)/
+const regExp = /(?:(timeright-)?(?:timeformat-(.+))?\W)/
 const fs = require('fs');
+const pageNavigationTimeout = 60000;
+const screenShot = false;
 
 const  _getPath = (folder) => `data//${folder}//`;
 
@@ -31,29 +33,26 @@ puppeteer.launch().then(async browser => {
   for (let test of tests) {
     errors = [];
     const page = await browser.newPage();
-    page.on("load", function() {
-      page.evaluate(function() {
-        (function () {
-          var log = console.error;
-          console.error = function () {
-            log.apply(this, ["ERROR:"].concat(Array.prototype.slice.call(arguments)));
-          };
-        }());
-      });
-    });
-    page.on('console', (...args) => {
-      if (args[0] === "ERROR:" && !errors.includes(args[1])) {
-        errors.push(args[1]);
-        console.error(...args);
+    page.setDefaultNavigationTimeout(pageNavigationTimeout);
+    page.on('console', async msg => {
+      if (msg.text().indexOf("AUTOCONFIG") !== -1) console.log(msg.text());
+      if (msg.type() === "error") {
+        const _msg = msg.args()[0];
+        const _prop = await _msg.getProperty("additionalInfo");
+        const _val = await _prop.jsonValue() || msg.text();
+        if (errors.indexOf(_val)) {
+          errors.push(_val);
+          console.log(`ERROR: ${_val}`);
+        }
       }
     });
     page.on('pageerror', (...args) => console.error('PAGE ERROR:', ...args));
     await console.log(`TEST(${test.folder}): page open for ${test.file}`);
-    await page.goto(host + "/bubblechart.html#" + test.urlParam, {waitUntil: 'networkidle', networkIdleTimeout: 15000, networkIdleInflight: 3});
-    //await page.screenshot({path: `${test.folder}-${test.file}.png`});
+    await page.goto(host + "/bubblechart.html#" + test.urlParam, {waitUntil: 'networkidle0'}).catch(error => console.error(error));
+    if (screenShot) await page.screenshot({path: `${test.folder}-${test.file}.png`});
     await page.close();
     await console.log(`TEST(${test.folder}): page close`);
   }
   await browser.close();
   await console.log('browser closed');
-});
+}, error => console.error(error));
