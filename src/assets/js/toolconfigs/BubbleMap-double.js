6d2e4571ea1d0cb2ddb45d461b47ff25d7ddd00b
Angie the Fairytale <konzeptmeister@gmail.com>
2020-02-06 17:57:43 +0100

    Update less-related tool configs

--- src/assets/js/toolconfigs/BubbleMap-double.js
+++ src/assets/js/toolconfigs/BubbleMap-double.js
@@ -1,127 +1,144 @@
 var __data = {
-  modelType: "ddfbw",
-  service: 'https://big-waffle.gapminder.org', 
-  dataset: 'unhcr',
-  name: "unhcr"
+    modelType: "ddfbw",
+    service: 'https://big-waffle.gapminder.org',
+    dataset: 'unhcr-only-refugees',
+    name: "unhcr-only-refugees"
 
-  //modelType: "ddfcsv",
-  //path: "./data/ddf--unhcr--population_statistics"
+    //modelType: "ddfcsv",
+    //path: "./data/ddf--unhcr--population_statistics"
 };
 
 var VIZABI_MODEL = {
-  model: {
-    markers: {
-        marker_destination: {
-        data: {
-            locale: "en",
-            source: __data,
-            space: ["geo", "year"]
-        },
-        encoding: {
-            "selected": {
-            modelType: "selection"
+    model: {
+        markers: {
+            marker_destination: {
+                data: {
+                    locale: "en",
+                    source: __data,
+                    space: ["geo", "year"]
+                },
+                encoding: {
+                    "selected": {
+                        modelType: "selection"
+                    },
+                    "highlighted": {
+                        modelType: "selection"
+                    },
+                    "size": {
+                        data: {
+                            concept: "population_arrived"
+                        }
+                    },
+                    "lat": {
+                        data: {
+                            space: ["geo"],
+                            concept: "latitude"
+                        }
+                    },
+                    "lon": {
+                        data: {
+                            space: ["geo"],
+                            concept: "longitude"
+                        }
+                    },
+                    "color": {
+                        data: {
+                            space: ["geo"],
+                            concept: "world_4region"
+                        },
+                        scale: {
+                            type: "ordinal",
+                            domain: ["europe", "asia", "africa", "americas"],
+                            range: ["#ffe700", "#ff5872", "#00d5e9", "#7feb00"]
+                        }
+                    },
+                    "label": {
+                        data: {
+                            space: ["geo"],
+                            concept: "name"
+                        }
+                    },
+                    "frame": {
+                        modelType: "frame",
+                        speed: 200,
+                        value: 2005,
+                        data: {
+                            concept: "year"
+                        }
+                    }
+                },
+                requiredEncodings: ["lat", "lon", "size"]
             },
-            "highlighted": {
-            modelType: "selection"
-            },
-            "size": {
-            data: {
-                concept: "population_arrived"
-            }
-            },
-            "lat": {
-            data: {
-                space: ["geo"],
-                concept: "latitude"
-            }
-            },
-            "lon": {
-            data: {
-                space: ["geo"],
-                concept: "longitude"
-            }
-            },
-            "color": {
-            data: {
-                space: ["geo"],
-                concept: "world_4region"
-            },
-            scale: {
-                type: "ordinal"
-            }
-            },
-            "label": {
-            data: {
-                space: ["geo"],
-                concept: "name"
-            }
-            },
-            "frame": {
-            modelType: "frame",
-            speed: 200,
-            value: 2005,
-            data: {
-                concept: "year"
-            }
+            marker_origin: {
+                data: {
+                    locale: "en",
+                    source: __data,
+                    space: ["geo", "year"]
+                },
+                encoding: {
+                    "selected": {
+                        modelType: "selection"
+                    },
+                    "highlighted": {
+                        modelType: "selection"
+                    },
+                    "size": {
+                        data: {
+                            concept: "population_left"
+                        }
+                    },
+                    "lat": {
+                        data: {
+                            space: ["geo"],
+                            concept: "latitude"
+                        }
+                    },
+                    "lon": {
+                        data: {
+                            space: ["geo"],
+                            concept: "longitude"
+                        }
+                    },
+                    "color": {
+                        data: {
+                            space: ["geo"],
+                            concept: "world_4region"
+                        },
+                        scale: {
+                            type: "ordinal",
+                            domain: ["europe", "asia", "africa", "americas"],
+                            range: ["#ffe700", "#ff5872", "#00d5e9", "#7feb00"]
+                        }
+                    },
+                    "label": {
+                        data: {
+                            space: ["geo"],
+                            concept: "name"
+                        }
+                    },
+                    "frame": {
+                        modelType: "frame",
+                        speed: 200,
+                        value: 2005,
+                        data: {
+                            concept: "year"
+                        }
+                    }
+                },
+                requiredEncodings: ["lat", "lon", "size"]
             }
         }
-        },
-        marker_origin: {
-        data: {
-            locale: "en",
-            source: __data,
-            space: ["geo", "year"]
-        },
-        encoding: {
-            "selected": {
-            modelType: "selection"
-            },
-            "highlighted": {
-            modelType: "selection"
-            },
-            "size": {
-            data: {
-                concept: "population_left"
-            }
-            },
-            "lat": {
-            data: {
-                space: ["geo"],
-                concept: "latitude"
-            }
-            },
-            "lon": {
-            data: {
-                space: ["geo"],
-                concept: "longitude"
-            }
-            },
-            "color": {
-            data: {
-                space: ["geo"],
-                concept: "world_4region"
-            },
-            scale: {
-                type: "ordinal"
-            }
-            },
-            "label": {
-            data: {
-                space: ["geo"],
-                concept: "name"
-            }
-            },
-            "frame": {
-            modelType: "frame",
-            speed: 200,
-            value: 2005,
-            data: {
-                concept: "year"
-            }
-            }
-        }
-        }
+    },
+    ui: {
+        FRAME: "year",
+        ORIGIN: "origin",
+        DESTINATION: "destination",
+        ENCODING: "size",
+        GEO: "geo",
+        ORIGIN_MEASURE: "population_left",
+        DESTINATION_MEASURE: "population_arrived",
+        GEO_MEASURE: "population_moved",
+        TITLE_FROM: "Refugees from:",
+        TITLE_TO: "Refugees to:"
     }
-  },
-  ui: {}
 };
--- src/assets/js/toolconfigs/IgnoReports-reactive.js
+++ src/assets/js/toolconfigs/IgnoReports-reactive.js
@@ -1,28 +1,33 @@
 var __data = {
-    modelType: "ddfbw",
-    service: "https://big-waffle.gapminder.org",
-    dataset: "surveys",
-    name: "surveys"
-  };
-  
-  var VIZABI_MODEL = {
-    model: {
-      data: {
-        locale: "en",
-        source: __data,
-        space: ["question", "survey"],
-        filter: {"dimensions": 
-          {"question": {"question.q_type": "abc"}}
-        }
-      },
-      encoding: {
-        "result": {
-          data: {
-            concept: "result"
+  modelType: "ddfbw",
+  service: "https://big-waffle.gapminder.org",
+  dataset: "surveys",
+  name: "surveys"
+};
+
+var VIZABI_MODEL = {
+  model: {
+    markers: {
+      marker_result: {
+        data: {
+          locale: "en",
+          source: __data,
+          space: ["question", "survey"],
+          filter: {
+            "dimensions":
+              { "question": { "question.q_type": "abc" } }
+          }
+        },
+        encoding: {
+          "result": {
+            data: {
+              concept: "result"
+            }
           }
         }
       }
-    },
-    ui: {
     }
+  },
+  ui: {
   }
+}
