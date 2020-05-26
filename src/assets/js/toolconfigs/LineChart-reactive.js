e11a470213eceabcc0153091dd1dd54c111e4899
Dmitry Basov <dmbasov@gmail.com>
2020-05-26 22:51:38 +0300

    update linechart, bubblechart configs

--- src/assets/js/toolconfigs/LineChart-reactive.js
+++ src/assets/js/toolconfigs/LineChart-reactive.js
@@ -5,67 +5,106 @@ var __data = {
 
 var VIZABI_MODEL = {
   model: {
-    requiredEncodings: ["x","y"],
-    data: {
-      locale: "en",
-      source: __data,
-      space: {
-        autoconfig: {
-          concept: {
-            $nin: ["age"]
-          }
-        }
-      }
-    },
-    encoding: {
-      "selected": {
-        modelType: "selection"
-      },
-      "highlighted": {
-        modelType: "selection"
-      },
-      "y": {
+    markers: {
+      line: {
+        requiredEncodings: ["x","y"],
         data: {
-          concept: "population_total"
-        },
-        scale: {
-          type: "log"
-        }
-      },
-      "x": {
-        data: {
-          concept: {            
+          filter: {
+            dimensions: {
+              "country": {
+                "country": {
+                  $in: ["usa","chn","rus","nga"]
+                },
+                //"country.un_state": true
+              }
+            }
+          },
+          locale: "en",
+          source: __data,
+          space: {
             autoconfig: {
-              concept_type: "time"
+              concept: {
+                $nin: ["age"]
+              }
+            }
+          }
+        },
+        encoding: {
+          "selected": {
+            modelType: "selection"
+          },
+          "highlighted": {
+            modelType: "selection"
+          },
+          "y": {
+            data: {
+              concept: "income_per_person_gdppercapita_ppp_inflation_adjusted"
+            },
+            scale: {
+              type: "log"
+            }
+          },
+          "x": {
+            data: {
+              concept: {            
+                autoconfig: {
+                  concept_type: "time"
+                }
+              }
+            }
+          },
+          "color": {
+            data: {
+              space: ["country"],
+              concept: "world_4region"
+            },
+            scale: {
+              modelType: "color",
+              type: "ordinal"
+            }
+          },
+          "label": {
+            data: {
+              //space: ["country"],
+              modelType: "entityPropertyDataConfig",
+              concept: "name"
+            }
+          },
+          frame: {
+            modelType: "frame",
+            speed: 200,
+            data: {
+              concept: {
+                autoconfig: {
+                  concept_type: "time"
+                }
+              }
             }
           }
         }
       },
-      "color": {
+      "legend": {
         data: {
-          space: ["country"],
-          concept: "world_4region"
-        },
-        scale: {
-          type: "ordinal"
-        }
-      },
-      "label": {
-        data: {
-          //space: ["country"],
-          modelType: "entityPropertyDataConfig",
-          concept: "name"
-        }
-      },
-      frame: {
-        modelType: "frame",
-        speed: 200,
-        data: {
-          concept: {
-            autoconfig: {
-              concept_type: "time"
-            }
+          ref: {
+            transform: "entityConceptSkipFilter",
+            model: "markers.line.encoding.color"
           }
+        },
+        encoding: {
+          color: {
+            data: { 
+              concept: { ref: "markers.line.encoding.color.data.concept" },
+              constant: { ref: "markers.line.encoding.color.data.constant" }
+            },
+            scale: {
+              modelType: "color",
+              palette: { ref: "markers.line.encoding.color.scale.palette" }
+            }
+            //scale: { ref: "markers.line.encoding.color.scale" }
+          },
+          name: { data: { concept: "name" } },
+          rank: { data: { concept: "rank" } },
+          map: { data: { concept: "shape_lores_svg" } }
         }
       }
     }
@@ -79,9 +118,17 @@ var VIZABI_MODEL = {
       "buttons": ["colors", "find", "moreoptions", "presentation", "sidebarcollapse", "fullscreen"],
     },
     "dialogs": {
-      "popup": ["timedisplay", "colors", "find", "moreoptions"],
-      "sidebar": ["timedisplay", "colors", "find"],
-      "moreoptions": ["opacity", "speed", "colors", "presentation", "about"]
+      "dialogs": {
+        "popup": ["colors", "find", "moreoptions"],
+        "sidebar": ["colors", "find"],
+        "moreoptions": ["opacity", "speed", "colors", "axes", "presentation", "about"]
+      },
+      "find": {
+        "panelMode":"show",
+        "showTabs": {
+          "country": "open"
+        }
+      }
     } 
   }
 }
